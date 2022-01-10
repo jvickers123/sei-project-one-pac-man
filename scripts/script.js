d79dcf96a63fd3717db5b26fe3ffa7e7ee12dc66
Jonny Vickers <jonathanbvickers@gmail.com>
2022-01-10 11:13:42 +0000

    fixed reset game bug

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,27 @@
 function init() {
   //elements
-  //  target grid
   const grid = document.querySelector('#grid')
-  //  target score display
   const scoreDisplay = document.querySelector('#score')
-  //  target high score display
   const highScoreDisplay = document.querySelector('#high-score')
-  //  target lives 
   const livesDisplay = document.querySelector('#lives')
-  //  target start button
   const startBtn = document.querySelector('#start-game')
 
   //variables
-  //  const width
   const width = 21
   const height = 21
-  //  const cell count
   const cellCount = width * height
-  //  const cells - empty array
   const cells = []
 
-  //  let score = 0
   let score = 0
   //  let high score = local storage
-  //  let lives = 3
   let lives = 3
   
 
   //classes
-  //  const wall class 
   const wallClass = 'wall'
   const gateClass = 'gate'
-  //  food class
   const foodClass = 'food'
-  //  ghost class
   const ghostClass = 'ghost'
-  //  pac man class
   const pacManClass = 'pac-man'
 
   //save board arrays
@@ -44,16 +30,11 @@ function init() {
   const wallsStartingPosition = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 22, 31, 40, 43, 45, 46, 48, 49, 50, 52, 54, 55, 56, 58, 59, 61, 64, 82, 85, 87, 88, 90, 92, 93, 94, 95, 96, 98, 100, 101, 103, 106, 111, 115, 119, 124, 127, 128, 129, 130, 132, 133, 134, 136, 138, 139, 140, 142, 143, 144, 145, 151, 153, 161, 163, 168, 169, 170, 171, 172, 174, 176, 177, 178, 179, 180, 182, 184, 185, 186, 187, 188, 197, 201, 210, 211, 212, 213, 214, 216, 218, 219, 220, 221, 222, 224, 226, 227, 228, 229, 230, 235, 237, 245, 247, 253, 254, 255, 256, 258, 260, 261, 262, 263, 264, 266, 268, 269, 270, 271, 274, 283, 292, 295, 297, 298, 300, 301, 302, 304, 306, 307, 308, 310, 311, 313, 316, 319, 331, 334, 337, 338, 340, 342, 344, 345, 346, 347, 348, 350, 352, 354, 355, 358, 363, 367, 371, 376, 379, 381, 382, 383, 384, 385, 386, 388, 390, 391, 392, 393, 394, 395, 397, 400, 418, 421, 422, 423, 424, 425, 426, 427, 428, 429, 430, 431, 432, 433, 434, 434, 435, 436, 437, 438, 439]
   const gate = 178
  
-  //  const pac man starting position
   const pacManStartingPosition = 325
-  //  const ghosts starting positions - an array
   const ghostsStartingPosition = [157, 198, 199, 200]
-  //  let pac man current position = pac man starting position
   let pacManCurrentPosition
-  //  let ghosts current position = ghosts starting position - an array
-  let ghostsCurrentPositon = []
+  const ghostsCurrentPositon = []
   const ghostDirection = []
-  // set playing game to active
   let playing = false
   
 
@@ -67,60 +48,19 @@ function init() {
   //    highscoreDOM = highscore
   //    make grid()
 
-  //make grid
-  function setGame() {
-    scoreDisplay.innerText = score
-    livesDisplay.innerText = lives
-    pacManCurrentPosition = pacManStartingPosition
-    ghostDirection.splice(0) //clears ghost direction
-    ghostDirection.push(null) // adds direction for first ghost
-    grid.style.width = `${width * 1.25}rem`// make grid based on width
-    grid.style.height = `${height * 1.25}rem`
-    grid.style.gridTemplateRows = `repeat(${height}, 1fr)`
-    grid.style.gridTemplateColumns = `repeat(${width}, 1fr)`
-    // for loop to create cells
-    for (let i = 0; i < cellCount; i++) {
-      const cell = document.createElement('div')
-      //make id of row and column position and x and y value in div
-      const cellXPosition = i % width
-      const cellYPosition = Math.floor(i / width)
-      cell.setAttribute('x', cellXPosition)
-      cell.setAttribute('y', cellYPosition)
-      cell.id = i
-      grid.appendChild(cell)
-      // push to empty cells array
-      cells.push(cell)
-    }
-    addPacMan(pacManCurrentPosition)
-    wallsStartingPosition.forEach(position => addWalls(position))
-    addGate(gate)
-    addFood()
-    ghostsStartingPosition.forEach((position, index) => addGhost(position, index))
-    ghostsStartingPosition.forEach(position => ghostsCurrentPositon.push(position))// let current position = starting position
-  }
-  setGame()
+  const addPacMan = (position) => cells[position].classList.add(pacManClass) //  add pac man class to current position
+  const addGate = (position) => cells[position].classList.add(gateClass)
+  const addWalls = (position) => cells[position].classList.add(wallClass)
+  const removePacMan = (position) => cells[position].classList.remove(pacManClass) //  remove pac man class from current position
 
-
-  function addWalls(position) {
-    cells[position].classList.add(wallClass)
-  }
-  
-  function addGate(position) {
-    cells[position].classList.add(gateClass)
-  }
-
-  function addFood() {
-    //filter cells array by if they do not contain wall class and they are not on the edge or in middle
-    const foodArray = cells.filter(cell => {
-      //get x and y coordinate for each cell
+  const addFood = () => {
+    const foodArray = cells.filter(cell => { //filter cells array by if they do not contain wall class and they are not on the edge or in middle
       const cellX = parseInt(cell.getAttribute('x'))
       const cellY = parseInt(cell.getAttribute('y'))
-
       const containsWall = cell.classList.contains('wall')
       const outside = (cellX === 0) || (cellX === width - 1)
       const middle = cellX >= (width / 7 * 2) && cellX <= (width / 3 * 2) && cellY >= (width / 3) && (cellY <= (width / 7 * 4))
       const extraSideSection = ( cellX <= 4 || cellX >= (width - 5) ) && ( cellY >= 6 && cellY <= 12 )
-
       if (!containsWall && !outside && !middle && !extraSideSection) {
         return true
       }
@@ -128,20 +68,7 @@ function init() {
     foodArray.forEach(cell => cell.classList.add(foodClass))
   }
 
-  //  add pac man(position)
-  function addPacMan(position) {
-    //add pac man class to cells[position]
-    cells[position].classList.add(pacManClass)
-  }
-
-  //  remove pac man(position)
-  function removePacMan(position) {
-    //remove pac man class to cells[position]
-    cells[position].classList.remove(pacManClass)
-  }
-
-  //  add ghosts (position, index)
-  function addGhost (position, index) {
+  const addGhost = (position, index) => {
     let colour
     if (index % 4 === 0) {
       colour = 'red'
@@ -155,8 +82,7 @@ function init() {
     cells[position].classList.add(ghostClass, colour)
   }
 
-  //  remove ghosts(position)
-  function removeGhosts(position, index) {
+  const removeGhosts = (position, index) => {
     let colour
     if (index % 4 === 0) {
       colour = 'red'
@@ -170,6 +96,56 @@ function init() {
     cells[position].classList.remove(ghostClass, colour)
   }
 
+  const setGame = () => {
+    //reset game
+    scoreDisplay.innerText = score
+    livesDisplay.innerText = lives
+    pacManCurrentPosition = pacManStartingPosition
+    ghostDirection.splice(0) //clears ghost direction
+    ghostDirection.push(null) // adds direction for first ghost
+    ghostsCurrentPositon.splice(0)
+    for (let i = 0; i < cellCount; i++) { // remove previous cells from grid
+      const cell = document.querySelector('.cell')
+      cell ? grid.removeChild(cell) : null
+    }
+    cells.splice(0) // empty cells array
+
+    //create grid
+    grid.style.width = `${width * 1.25}rem`// make grid based on width
+    grid.style.height = `${height * 1.25}rem`
+    grid.style.gridTemplateRows = `repeat(${height}, 1fr)`
+    grid.style.gridTemplateColumns = `repeat(${width}, 1fr)`
+
+    // create cells
+    for (let i = 0; i < cellCount; i++) { 
+      const cell = document.createElement('div')
+      const cellXPosition = i % width //get x and y coordinates of cell
+      const cellYPosition = Math.floor(i / width)
+      cell.setAttribute('x', cellXPosition) // set coordinates as attributes to cell
+      cell.setAttribute('y', cellYPosition)
+      cell.id = i
+      cell.classList.add('cell')
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+    //add classes
+    addPacMan(pacManCurrentPosition)
+    wallsStartingPosition.forEach(position => addWalls(position))
+    addGate(gate)
+    addFood()
+    ghostsStartingPosition.forEach((position, index) => addGhost(position, index))
+    ghostsStartingPosition.forEach(position => ghostsCurrentPositon.push(position))// let current position = starting position
+  }
+  setGame()
+  
+  const checkLives = (cell, checkClass) => {
+    if (cell.classList.contains(checkClass)) {
+      lives--
+      livesDisplay.innerHTML = lives
+    }
+    lives < 0 ? endGame('lose') : null
+  }
+
   //  move pac man(e)
   function movePacMan(e) {
     if (playing === true) {
@@ -179,11 +155,9 @@ function init() {
       const left = 37
       const up = 38
       const down = 40
-      // remove pac man(pac man current position)
       removePacMan(pacManCurrentPosition)
       // if to check direction of key, whether the next square is a wall or edge of grid
       if (key === right && !cells[pacManCurrentPosition + 1].classList.contains(wallClass)) {
-        //pac man current position row or column change
         pacManCurrentPosition++
       } else if (key === left && !cells[pacManCurrentPosition - 1].classList.contains(wallClass)) {
         pacManCurrentPosition--
@@ -192,7 +166,6 @@ function init() {
       } else if (key === down && !cells[pacManCurrentPosition + width].classList.contains(wallClass)) {
         pacManCurrentPosition += width
       }
-      // add pac man(pac man current position)
       addPacMan(pacManCurrentPosition)
       const currentCell = cells[pacManCurrentPosition]
       // if current position has food class
@@ -202,16 +175,12 @@ function init() {
         scoreDisplay.innerText = score
       }
       // if current position has ghost class
-      if (currentCell.classList.contains(ghostClass)) {
-        lives--
-        livesDisplay.innerHTML = lives
-      }
-      lives < 0 ? endGame('lose') : null
+      checkLives(currentCell, ghostClass)
       cells.some(cell => cell.classList.contains(foodClass)) ? null : endGame('win')
     }
   }
   
-  function moveGhosts() {
+  const  moveGhosts = () =>{
     const directionOptions = ['up', 'down', 'left', 'right']
     ghostsCurrentPositon.forEach((position, index) => {
       removeGhosts(position, index)
@@ -232,7 +201,7 @@ function init() {
         return direction
       }
       pickDirection(ghostDirection[index])
-      const movingGhost = (direction) => {
+      const movingGhost = (direction) => { //changes current position unless next cell is wall otherwise picks a new direction 
         if (direction === 'right') {
           !cells[position + 1].classList.contains(wallClass) ? ghostsCurrentPositon[index]++ : pickDirection(null)
         } else if (direction === 'left') {
@@ -245,6 +214,7 @@ function init() {
         addGhost(ghostsCurrentPositon[index], index)
       }
       movingGhost(ghostDirection[index])
+      checkLives(cells[ghostsCurrentPositon[index]], pacManClass)
     })
   }
 
@@ -252,7 +222,7 @@ function init() {
   let releaseGhostsInterval
   let releaseGhostCount = 1 //give global scope so it can be reset in endgame
 
-  function releaseGhosts() {
+  const releaseGhosts = () => {
     if (releaseGhostCount < ghostsStartingPosition.length) {// if count is lower than no. ghosts
       const index = releaseGhostCount
       removeGhosts(ghostsCurrentPositon[index], index)
@@ -266,22 +236,22 @@ function init() {
     
   }
 
-  function playGame() {
-    // playing = true
+  const playGame = () => {
+    playing = true
     moveGhostInterval = setInterval(moveGhosts, 500) // start moving ghosts
     releaseGhostsInterval = setInterval(releaseGhosts, 1000 * 7) // release ghosts every 7 seconds
   }
     
   
   //  end game(result)
-  function endGame(result) {
+  const endGame = (result) => {
     clearInterval(moveGhostInterval)
     clearInterval(releaseGhostsInterval)
     result === 'lose' ?  alert('you lose', score) : alert('you win', score)
     startBtn.innerText = 'restart'
     removePacMan(pacManCurrentPosition)
     ghostsCurrentPositon.forEach((position, index) => removeGhosts(position, index))
-    ghostsCurrentPositon = []
+    // ghostsCurrentPositon
     lives = 3
     score = 0
     playing = false
